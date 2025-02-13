c75c16830af60b6299978e538647fee8df21082b
ny-555 <nywork.fb1234@gmail.com>
2025-02-13 22:40:55 +0900

    feat:データベースの追加20%

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,10 @@
 import { NextAuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-// import Google from "next-auth/providers/google";
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -15,6 +17,9 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+
+  adapter: PrismaAdapter(prisma),
+
   pages: {
     signIn: "/login",
   },
