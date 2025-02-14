73898e166ac27521e16c364d529a91c5725f3745
ny-555 <nywork.fb1234@gmail.com>
2025-02-14 21:51:54 +0900

    feat:ダッシュボードの作成20%

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+import MainNav from "@/components/main-nav";
+import SiteFooter from "@/components/site-footer";
+import { dashboardConfig } from "@/config/dashboard";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col space-y-6">
+      <header className="sticky top-0 z-40 border-b bg-background">
+        <div className="container mx-zuto px-8 flex items-center justify-between py-4 h-16">
+          <MainNav items={dashboardConfig.mainNav} />
+        </div>
+      </header>
+      <div className="container mx-auto px-8 grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+        <aside className="hidden md:flex w-[200px] flex-col">Sideber</aside>
+        <main className="flex flex-col w-full flex-1 overflow-hidden">
+          {children}
+        </main>
+      </div>
+
+      <SiteFooter />
+    </div>
+  );
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -9,10 +9,12 @@ export const authOptions: NextAuthOptions = {
     Github({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
 
@@ -32,5 +34,9 @@ export const authOptions: NextAuthOptions = {
 
       return session;
     },
+  },
+
+  session: {
+    strategy: "jwt",
   },
 };
