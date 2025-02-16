f66cf9f9bfae41c0038b07eb7564be9260040356
ny-555 <nywork.fb1234@gmail.com>
2025-02-16 21:55:56 +0900

    feat:記事投稿機能の追加80%

---
+++ components/editor.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import TextareaAutosize from "react-textarea-autosize";
+import EditorJS from "@editorjs/editorjs";
+import { useCallback, useEffect, useRef, useState } from "react";
+import Header from "@editorjs/header";
+import LinkTool from "@editorjs/link";
+import List from "@editorjs/list";
+import Code from "@editorjs/code";
+import { Post } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { postPatchSchema, postPatchSchemaType } from "@/lib/validations/post";
+
+interface EditorProps {
+  post: Pick<Post, "id" | "title" | "content" | "published">;
+}
+
+export default function Editor({ post }: EditorProps) {
+  const ref = useRef<EditorJS | undefined>(null);
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  const initializeEditor = useCallback(async () => {
+    const editor = new EditorJS({
+      holder: "editor",
+      onReady() {
+        ref.current = editor;
+      },
+      placeholder: "ここに記事を書く",
+      inlineToolbar: true,
+      tools: {
+        header: Header,
+        LinkTool: LinkTool,
+        list: List,
+        code: Code,
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      initializeEditor();
+    }
+
+    return () => {
+      ref.current?.destroy();
+      ref.current = undefined;
+    };
+  }, [isMounted, initializeEditor]);
+
+  const { register, handleSubmit } = useForm<postPatchSchemaType>({
+    resolver: zodResolver(postPatchSchema),
+  });
+
+  const onSubmit = async (data: postPatchSchemaType) => {
+    const blocks = await ref.current?.save();
+    console.log(data);
+    console.log(blocks);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid w-full gap-10">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center space-x-10">
+            <Link
+              href={"/dashboard"}
+              className={cn(buttonVariants({ variant: "ghost" }))}
+            >
+              戻る
+            </Link>
+            <p className="text-sm text-muted-foreground">公開</p>
+          </div>
+          <button className={cn(buttonVariants())} type="submit">
+            <span>保存</span>
+          </button>
+        </div>
+        <div className="w-[800px] mx-auto">
+          <TextareaAutosize
+            id="title"
+            autoFocus
+            defaultValue={post.title}
+            placeholder="Post Title"
+            className="w-full resize-none overflow-hidden bg-transparent text-4xl focus:outline-none font-bold"
+            {...register("title")}
+          />
+        </div>
+        <div id="editor" className="min-h-[500px]" />
+        <p className="text-sm text-gray-500">
+          Use
+          <kbd className="rounded-md border bg-muted px-1 text-xs uppercase">
+            Tab
+          </kbd>
+          to open the command menu
+        </p>
+      </div>
+    </form>
+  );
+}
--- components/post-create-button.tsx
+++ components/post-create-button.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils";
 import { ButtonProps, buttonVariants } from "./ui/button";
 import { useState } from "react";
 import { Icon } from "./icon";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
 
 type PostCreateButtonProps = ButtonProps;
 
@@ -12,14 +14,44 @@ export default function PostCreateButton({
   variant,
   ...props
 }: PostCreateButtonProps) {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
 
-  const onClick = async () => {};
+  const onClick = async () => {
+    setIsLoading(true);
+
+    const response = await fetch("api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "Untitled Post",
+      }),
+    });
+
+    setIsLoading(false);
+
+    if (!response.ok) {
+      return toast({
+        title: "問題が発生しました。",
+        description: "投稿が作成されませんでした。もう一度お試しください。",
+        variant: "destructive",
+      });
+    }
+
+    const post = await response.json();
+
+    router.refresh();
+    router.push(`editor/${post.id}`);
+  };
 
   return (
     <button
       className={cn(buttonVariants({ variant }), {
         "cursor-not-allowed opacity-60": isLoading,
+        className,
       })}
       onClick={onClick}
       disabled={isLoading}
