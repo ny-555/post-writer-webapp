75b4ca238b74747f6d6741704c638b760b348683
ny-555 <nywork.fb1234@gmail.com>
2025-02-22 22:37:51 +0900

    ref:リファクタリング

---
+++ prisma/migrations/20250222133557_init/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated_at_at` on the `posts` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[stripe_customer_id]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[stripe_subscription_id]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "posts" DROP COLUMN "updated_at_at",
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "stripe_current_period_end" TIMESTAMP(3),
+ADD COLUMN     "stripe_customer_id" TEXT,
+ADD COLUMN     "stripe_price_id" TEXT,
+ADD COLUMN     "stripe_subscription_id" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_stripe_customer_id_key" ON "users"("stripe_customer_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_stripe_subscription_id_key" ON "users"("stripe_subscription_id");
